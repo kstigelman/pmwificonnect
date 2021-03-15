19e72d8907971abe90862efa976a27f56f38f6b0
kstigelman <kystiges23@gmail.com>
2021-03-15 08:44:10 -0400

    Ping command

--- main.py
+++ main.py
@@ -7,8 +7,9 @@ command = os.popen("ping -c 1 homepage.pennmanor.net").readlines()
 ping_line = command[5]
 
 index = ping_line.rfind("/", 0, ping_line.rfind("/") - 1)
-ping = int(ping_line[index+1:ping_line.rfind("/")])
+ping = ping_line[index+1:ping_line.rfind("/")]
 
-if ping > 150:
-    print("Connection error!")
+
+print("Connection error!")
+
 print(ping)
