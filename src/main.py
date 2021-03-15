d50085dd055a6479270150fef52710ac12ec230c
Horst26 <201503@pennmanor.net>
2021-03-15 08:48:49 -0400

    Conditional

--- src/main.py
+++ src/main.py
@@ -1,2 +1,18 @@
+#Luke Horst and Kyler Stigelman
 
+import os
+import string
+
+command = os.popen("ping -c 1 homepage.pennmanor.net").readlines()
+ping_line = command[5]
+
+index = ping_line.rfind("/", 0, ping_line.rfind("/") - 1)
+ping = ping_line[index+1:ping_line.rfind("/")]
+
+if float(ping) > 250:
+    print("Connection error!")
+else:
+    print('Ok')
+
+print(ping)
 
