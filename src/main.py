15105e7e96ff073b42d98167c57ed548d1483f6f
Horst26 <201503@pennmanor.net>
2021-03-16 07:53:47 -0400

    Comments

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 #Luke Horst and Kyler Stigelman
 
+#repeat the ping command for every iteration of the program that is run
 import os
 
 command = os.popen("ping -c 1 homepage.pennmanor.net").readlines()
@@ -8,6 +9,7 @@ ping_line = command[5]
 index = ping_line.rfind("/", 0, ping_line.rfind("/") - 1)
 ping = ping_line[index+1:ping_line.rfind("/")]
 
+#if the ping is too high, have a system that changes the network/connections
 if float(ping) > 150:
     print("Connection error!")
 else:
