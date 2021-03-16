56b90fe64f445f17b8990b241d2a29c4e2add9d5
Horst26 <201503@pennmanor.net>
2021-03-16 08:07:47 -0400

    Function

--- src/main.py
+++ src/main.py
@@ -3,17 +3,17 @@
 #repeat the ping command for every iteration of the program that is run
 import os
 
+def test():
+    command = os.popen("ping -c 1 homepage.pennmanor.net").readlines()
+    ping_line = command[5]
 
-command = os.popen("ping -c 1 homepage.pennmanor.net").readlines()
-ping_line = command[5]
+    index = ping_line.rfind("/", 0, ping_line.rfind("/") - 1)
+    ping = ping_line[index+1:ping_line.rfind("/")]
 
-index = ping_line.rfind("/", 0, ping_line.rfind("/") - 1)
-ping = ping_line[index+1:ping_line.rfind("/")]
-
-#if the ping is too high, have a system that changes the network/connections
-if float(ping) > 150:
-    print("Connection error! Ping is", ping)
-    os.popen("sudo service network-manager restart")
-else:
-    print("Connection ok")
-
+    if float(ping) > 150:
+        return("Connection error! Ping is", ping)
+        os.popen("sudo service network-manager restart")
+        test()
+    else:
+        return("Connection ok")
+print(test())
