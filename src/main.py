d5e615bb611d43c8c90e5d6b8d42905f5fff5f7a
kstigelman <kystiges23@gmail.com>
2021-03-16 08:33:57 -0400

    Added case for no connection

--- src/main.py
+++ src/main.py
@@ -5,15 +5,19 @@ import os
 
 def test():
     command = os.popen("ping -c 1 homepage.pennmanor.net").readlines()
-    ping_line = command[5]
+    if command != "ping: homepage.pennmanor.net: Name or service not known":
+        ping_line = command[5]
+        index = ping_line.rfind("/", 0, ping_line.rfind("/") - 1)
+        ping = ping_line[index+1:ping_line.rfind("/")]
 
-    index = ping_line.rfind("/", 0, ping_line.rfind("/") - 1)
-    ping = ping_line[index+1:ping_line.rfind("/")]
-
-    if float(ping) > 150:
+        if float(ping) > 150:
+            print("Connection error!")
+            os.popen("sudo service network-manager restart")
+            test()
+        else:
+            return "Connection ok"
+    else:
         print("Connection error!")
         os.popen("sudo service network-manager restart")
         test()
-    else:
-        return "Connection ok"
 print(test())
